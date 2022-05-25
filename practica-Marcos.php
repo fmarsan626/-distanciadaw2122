de797bb12885c1218dce231bfa68f6846db2993c
fmarsan626 <71500034+fmarsan626@users.noreply.github.com>
2022-05-25 12:30:49 +0200

    Primer commit
    Primer commint del ejercicio 2.3

---
+++ practica-Marcos.php
@@ -0,0 +1,24 @@
+<?php
+/**
+  *  @author Marcos
+  *  @version 1.0
+*/
+
+  /**
+    * Función suma de dos números
+    * @param int $num1
+    * @param int $num2
+    * @return float
+    * @internal  Con esta anotación se vuelve solo para desarrolladores
+  */
+
+  function suma ($num1, $num2) {
+      $resultadosuma = $num1 + $num2;
+      return $resultadosuma;
+
+  }
+
+$suma = suma (24,42);
+echo $suma;
+
+?>
